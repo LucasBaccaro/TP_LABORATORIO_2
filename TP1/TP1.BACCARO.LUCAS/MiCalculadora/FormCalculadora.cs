d068edd82d4c9fa72c6fc3d3e315c3a1b3aa47f3
LucasBaccaro <68014426+LucasBaccaro@users.noreply.github.com>
2021-04-18 20:34:19 -0300

    Create FormCalculadora.cs

---
+++ TP1/TP1.BACCARO.LUCAS/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            cmboOperador.Text = "+";
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            if (txtNum1.Text == string.Empty || txtNum2.Text == string.Empty)
+            {
+                MessageBox.Show("Para convertir a binario, ingrese numeros.");
+            }
+            {
+                Numero num = new Numero();
+                lblResultado.Text = num.DecimalBinario(lblResultado.Text);
+            }
+          
+
+        }
+        private void convertirADecimal_Click(object sender, EventArgs e)
+        {
+            if (txtNum1.Text == string.Empty || txtNum2.Text == string.Empty)
+            {
+                MessageBox.Show("Para convertir a decimal, ingrese numeros.");
+            }
+            else
+            {
+                Numero num = new Numero();
+                lblResultado.Text = num.BinarioDecimal(lblResultado.Text);
+            }
+               
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            if(txtNum1.Text == string.Empty || txtNum2.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese numeros para operar");
+            }else
+            {
+            string operacion;
+            operacion = Convert.ToString(Operar(txtNum1.Text, txtNum2.Text, cmboOperador.Text));
+            lblResultado.Text = operacion;
+            }
+            
+        }
+        private void Limpiar()
+        {
+            this.txtNum1.Clear();
+            this.txtNum2.Clear();
+            this.lblResultado.Text = null;
+
+        }
+        private static double Operar (string numero1,string numero2,string operador)
+        {
+            Numero num1 = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+            if (operador=="")
+            {
+                operador = "+";
+            }
+            double resultado = Calculadora.Operar(num1, num2, operador);
+            return resultado;
+        }  
+    }
+}
